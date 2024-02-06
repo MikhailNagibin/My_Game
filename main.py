39f84ba68cc73139469ea1f25d49af18e9d2eef9
nagib <nagibimi@gmail.com>
2024-02-06 22:04:15 +0300

    add particle

--- main.py
+++ main.py
@@ -16,42 +16,65 @@ ghost_group = pygame.sprite.Group()
 player_group = pygame.sprite.Group()
 box_group = pygame.sprite.Group()
 key_group = pygame.sprite.Group()
+particle_group = pygame.sprite.Group()
 golden_door_group = pygame.sprite.Group()
 global_pos = [2, 2]
 player_pos = [5, 5]
 portal_pos = [random.randint(0, 5), random.randint(0, 5)]
 tile_width = tile_height = 50
 pos_level = {
-    '00': "00.txt",
-    '01': "стена1.txt",
-    '02': "стена1.txt",
-    '03': "стена1.txt",
-    '04': "011.txt",
-    '10': "стена2.txt",
-    '11': "середина.txt",
-    '12': "середина.txt",
-    '13': "середина.txt",
-    '14': "стена4.txt",
-    '20': "стена2.txt",
-    '21': "середина.txt",
-    '22': "середина.txt",
-    '23': "середина.txt",
-    '24': "стена4.txt",
-    '30': "стена2.txt",
-    '31': "середина.txt",
-    '32': "середина.txt",
-    '33': "середина.txt",
-    '34': "стена4.txt",
-    '40': "110.txt",
-    '41': "стена3.txt",
-    '42': "стена3.txt",
-    '43': "стена3.txt",
-    '44': "1111.txt",
+    "00": "00.txt",
+    "01": "стена1.txt",
+    "02": "стена1.txt",
+    "03": "стена1.txt",
+    "04": "011.txt",
+    "10": "стена2.txt",
+    "11": "середина.txt",
+    "12": "середина.txt",
+    "13": "середина.txt",
+    "14": "стена4.txt",
+    "20": "стена2.txt",
+    "21": "середина.txt",
+    "22": "середина.txt",
+    "23": "середина.txt",
+    "24": "стена4.txt",
+    "30": "стена2.txt",
+    "31": "середина.txt",
+    "32": "середина.txt",
+    "33": "середина.txt",
+    "34": "стена4.txt",
+    "40": "110.txt",
+    "41": "стена3.txt",
+    "42": "стена3.txt",
+    "43": "стена3.txt",
+    "44": "1111.txt",
 }
-
-posible_global_pos_golden_door = [[0, 0], [0, 1], [0, 2], [0, 3], [0, 4], [0, 4], [1, 4], [2, 4], [3, 4], [4, 4],
-                                  [4, 4], [4, 3], [4, 2], [4, 1], [4, 0], [4, 0], [3, 0], [2, 0], [1, 0], [0, 0]]
-posible_local_pos_golden_door = [[0, 5]] * 5 + [[5, 10]] * 5 + [[10, 5]] * 5 + [[5, 0]] * 5
+pygame.mouse.set_visible(0)
+posible_global_pos_golden_door = [
+    [0, 0],
+    [0, 1],
+    [0, 2],
+    [0, 3],
+    [0, 4],
+    [0, 4],
+    [1, 4],
+    [2, 4],
+    [3, 4],
+    [4, 4],
+    [4, 4],
+    [4, 3],
+    [4, 2],
+    [4, 1],
+    [4, 0],
+    [4, 0],
+    [3, 0],
+    [2, 0],
+    [1, 0],
+    [0, 0],
+]
+posible_local_pos_golden_door = (
+    [[0, 5]] * 5 + [[5, 10]] * 5 + [[10, 5]] * 5 + [[5, 0]] * 5
+)
 pos_golden_door = random.randint(0, len(posible_global_pos_golden_door) - 1)
 filling_boxes = []
 aftomat_pos = random.randint(0, 100)
@@ -66,11 +89,12 @@ for i in range(15):
     key_local_pos.append(posible_local_pos[random.randint(0, 3)])
     print(key_global_pos[-1], key_local_pos[-1])
 golden_door_pos = random.randint(0, 24)
+screen_rect = (0, 0, 550, 550)
 
 
 def load_image(name, colorkey=None):
-    fullname = os.path.join('sprites', name)
-    fullname = os.path.join('data', fullname)
+    fullname = os.path.join("sprites", name)
+    fullname = os.path.join("data", fullname)
     image = pygame.image.load(fullname)
     if colorkey is not None:
         image = image.convert()
@@ -84,10 +108,10 @@ def load_image(name, colorkey=None):
 
 def load_level(filename):
     filename = "data/levels/" + filename
-    with open(filename, 'r') as mapFile:
+    with open(filename, "r") as mapFile:
         level_map = [line.strip() for line in mapFile]
     max_width = max(map(len, level_map))
-    return list(map(lambda x: x.ljust(max_width, '.'), level_map))
+    return list(map(lambda x: x.ljust(max_width, "."), level_map))
 
 
 tile_images = {
@@ -100,7 +124,7 @@ tile_images = {
     "key": load_image("new_key.png"),
     "golden_door": load_image("golden_door.jpg"),
     "start": load_image("start.jpg"),
-    "end": load_image("end.jpg")
+    "end": load_image("end.jpg"),
 }
 
 
@@ -112,8 +136,7 @@ def start_end_screen(tile):
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
-            elif event.type == pygame.KEYDOWN or \
-                    event.type == pygame.MOUSEBUTTONDOWN:
+            elif event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN:
                 return
         pygame.display.flip()
         clock.tick(FPS)
@@ -124,19 +147,20 @@ def generate_level(level):
     print(global_pos)
     for y in range(len(level)):
         for x in range(len(level[y])):
-            if level[y][x] == '*':
-                Flor('flor', x, y)
-            elif (level[y][x] == '#' and
-                  (global_pos != posible_global_pos_golden_door[pos_golden_door] or
-                   [y, x] != posible_local_pos_golden_door[pos_golden_door])):
-                Wall('wall', x, y)
-            elif level[y][x] == '@':
-                Flor('flor', x, y)
-            elif level[y][x] == 'D':
-                Flor('flor', x, y)
-                Door('door', x, y)
+            if level[y][x] == "*":
+                Flor("flor", x, y)
+            elif level[y][x] == "#" and (
+                global_pos != posible_global_pos_golden_door[pos_golden_door]
+                or [y, x] != posible_local_pos_golden_door[pos_golden_door]
+            ):
+                Wall("wall", x, y)
+            elif level[y][x] == "@":
+                Flor("flor", x, y)
+            elif level[y][x] == "D":
+                Flor("flor", x, y)
+                Door("door", x, y)
     if global_pos == posible_global_pos_golden_door[pos_golden_door]:
-        Flor('flor', *posible_local_pos_golden_door[pos_golden_door])
+        Flor("flor", *posible_local_pos_golden_door[pos_golden_door])
         Golden_door(posible_local_pos_golden_door[pos_golden_door])
     return x, y
 
@@ -144,17 +168,18 @@ def generate_level(level):
 class Golden_door(pygame.sprite.Sprite):
     def __init__(self, pos):
         super().__init__(golden_door_group, all_sprites)
-        self.image = tile_images['golden_door']
+        self.image = tile_images["golden_door"]
         if pos[0] != 5:
             self.image = pygame.transform.rotate(self.image, 90)
         self.rect = self.image.get_rect().move(
-            tile_width * pos[0], tile_height * pos[1])
+            tile_width * pos[0], tile_height * pos[1]
+        )
 
 
 class Keys(pygame.sprite.Sprite):
     def __init__(self, pos):
         super().__init__(key_group, all_sprites)
-        self.image = tile_images['key']
+        self.image = tile_images["key"]
         print(pos)
         self.rect = self.image.get_rect().move(pos)
 
@@ -163,16 +188,14 @@ class Flor(pygame.sprite.Sprite):
     def __init__(self, tile_type, pos_x, pos_y):
         super().__init__(flor_group, all_sprites)
         self.image = tile_images[tile_type]
-        self.rect = self.image.get_rect().move(
-            tile_width * pos_x, tile_height * pos_y)
+        self.rect = self.image.get_rect().move(tile_width * pos_x, tile_height * pos_y)
 
 
 class Wall(pygame.sprite.Sprite):
     def __init__(self, tile_type, pos_x, pos_y):
         super().__init__(wall_group, all_sprites)
         self.image = tile_images[tile_type]
-        self.rect = self.image.get_rect().move(
-            tile_width * pos_x, tile_height * pos_y)
+        self.rect = self.image.get_rect().move(tile_width * pos_x, tile_height * pos_y)
 
 
 class Door(pygame.sprite.Sprite):
@@ -182,15 +205,19 @@ class Door(pygame.sprite.Sprite):
         if pos_x != 5:
             self.image = pygame.transform.rotate(self.image, 90)
         self.rect = self.image.get_rect().move(
-            tile_width * pos_x + 30 * (pos_x == 10), tile_height * pos_y + 30 * (pos_y == 10))
+            tile_width * pos_x + 30 * (pos_x == 10),
+            tile_height * pos_y + 30 * (pos_y == 10),
+        )
 
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__(player_group, all_sprites)
-        self.image = tile_images['player']
+        self.image = tile_images["player"]
         self.direction = "Right"
-        self.rect = self.image.get_rect().move(player_pos[0] * 50 + 7, player_pos[1] * 50)
+        self.rect = self.image.get_rect().move(
+            player_pos[0] * 50 + 7, player_pos[1] * 50
+        )
 
     def update(self, delta, direction=None):
         if direction and direction != self.direction:
@@ -202,8 +229,14 @@ class Player(pygame.sprite.Sprite):
         self.rect.x, self.rect.y = self.rect.x + delta[0], self.rect.y + delta[1]
         if pygame.sprite.spritecollideany(self, door_group):
             return True
-        if (pygame.sprite.spritecollideany(self, wall_group) or pygame.sprite.spritecollideany(self, box_group) or
-                self.rect.x <= 0 or self.rect.x >= WIDTH or self.rect.y <= 0 or self.rect.y >= HEIGHT):
+        if (
+            pygame.sprite.spritecollideany(self, wall_group)
+            or pygame.sprite.spritecollideany(self, box_group)
+            or self.rect.x <= 0
+            or self.rect.x >= WIDTH
+            or self.rect.y <= 0
+            or self.rect.y >= HEIGHT
+        ):
             self.rect.x, self.rect.y = self.rect.x - delta[0], self.rect.y - delta[1]
 
     def check(self):
@@ -213,13 +246,20 @@ class Player(pygame.sprite.Sprite):
                 return False, True
             if pygame.sprite.spritecollideany(self, box_group):
                 box = pygame.sprite.spritecollideany(self, box_group)
-                if global_pos in key_global_pos and \
-                    [box.rect.x // 50, box.rect.y // 50] == key_local_pos[key_global_pos.index(global_pos)]:
+                if (
+                    global_pos in key_global_pos
+                    and [box.rect.x // 50, box.rect.y // 50]
+                    == key_local_pos[key_global_pos.index(global_pos)]
+                ):
+                    create_particles((box.rect.x, box.rect.y))
                     Keys(pygame.sprite.spritecollideany(self, box_group).rect[:2])
                     key_global_pos.remove(global_pos)
                     key_local_pos.remove([box.rect.x // 50, box.rect.y // 50])
-                print(global_pos, key_global_pos)
-                print([box.rect.x // 50, box.rect.y // 50], key_local_pos[key_global_pos.index(global_pos)])
+                # print(global_pos, key_global_pos)
+                # print(
+                #     [box.rect.x // 50, box.rect.y // 50],
+                #     key_local_pos[key_global_pos.index(global_pos)],
+                # )
             self.rect.x, self.rect.y = self.rect.x - delta[0], self.rect.y - delta[1]
         return True, False
 
@@ -227,14 +267,58 @@ class Player(pygame.sprite.Sprite):
 class Box(pygame.sprite.Sprite):
     def __init__(self, pos):
         super().__init__(box_group, all_sprites)
-        self.image = tile_images['box0']
+        self.image = tile_images["box0"]
         self.rect = self.image.get_rect().move(
-            tile_width * pos[0], tile_height * pos[1])
+            tile_width * pos[0], tile_height * pos[1]
+        )
 
 
-start_end_screen('start')
+class Particle(pygame.sprite.Sprite):
+    # сгенерируем частицы разного размера
+    fire = [load_image("new_key.png")]
+    for scale in (5, 10, 20):
+        fire.append(pygame.transform.scale(fire[0], (scale, scale)))
+
+    def __init__(self, pos, dx, dy):
+
+        super().__init__(all_sprites, particle_group)
+        self.image = random.choice(self.fire)
+        self.rect = self.image.get_rect()
+
+        # у каждой частицы своя скорость - это вектор
+        self.velocity = [dx, dy]
+        # и свои координаты
+        self.rect.x, self.rect.y = pos
+
+        # гравитация будет одинаковой
+        self.gravity = 0.25
+
+    def update(self):
+        # применяем гравитационный эффект:
+        # движение с ускорением под действием гравитации
+        self.velocity[1] += self.gravity
+        # перемещаем частицу
+        self.rect.x += self.velocity[0]
+        self.rect.y += self.velocity[1]
+        # убиваем, если частица ушла за экран
+        if not self.rect.colliderect(screen_rect):
+            self.kill()
+
+
+def create_particles(position):
+    # количество создаваемых частиц
+    particle_count = 20
+    # возможные скорости
+    numbers = range(-5, 6)
+    for _ in range(particle_count):
+        Particle(position, random.choice(numbers), random.choice(numbers))
+
+
+start_end_screen("start")
 start_of_game = time.time()
-level_x, level_y = generate_level(load_level(pos_level[str(global_pos[0]) + str(global_pos[1])]))
+level_x, level_y = generate_level(
+    load_level(pos_level[str(global_pos[0]) + str(global_pos[1])])
+)
 box_pos = [[1, 1], [9, 9], [1, 9], [9, 1]]
 boxes = []
 for i in range(4):
@@ -279,7 +363,8 @@ while running:
                 flor_group = pygame.sprite.Group()
                 door_group = pygame.sprite.Group()
                 level_x, level_y = generate_level(
-                    load_level(pos_level[str(global_pos[0]) + str(global_pos[1])]))
+                    load_level(pos_level[str(global_pos[0]) + str(global_pos[1])])
+                )
                 player.kill()
                 player = Player()
     all_sprites.draw(screen)
@@ -287,9 +372,10 @@ while running:
     player_group.draw(screen)
     box_group.draw(screen)
     key_group.draw(screen)
+    particle_group.update()
     pygame.display.flip()
     clock.tick(FPS)
 end_of_game = time.time()
 if end:
     print(round(end_of_game - start_of_game, 2))
-    start_end_screen('end')
+    start_end_screen("end")
